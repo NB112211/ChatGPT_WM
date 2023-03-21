725d2dc2d92041eda3844f072793052f01c3684e
Allen <103039975+NB112211@users.noreply.github.com>
2023-03-22 00:57:17 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
     <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+     <a href="https://t.me/Nb112211" target="_blank" class="text-blue-500">
+      Telegram
     </a>
   </div>
 </template>
